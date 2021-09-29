597b880264421e332ab69d23b277516daa01d37d
Sasha O'Donovan <sasha.odonovan@gmail.com>
2021-09-29 11:29:03 +0100

    flag page styled in grid with score and timer div

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// UI variables
+
+const nameInput = document.querySelector(".intro__nameinput");
+const flagImage = document.querySelector(".flag__image");
+
+// first page
+
+// input name and store in a variable
+// on click of button show next screen
+
+// second page
+
+// display instructions and onClick show flags
+
+// third page/flag page
+
+// show flags for X seconds
+// flip flags to solid
+
+// when user clicks flag, display
+// when user clicks 2nd flag, display
+
+// if flags match, keep as shown & add to score and show winning message with fact / flag image  - animate flags so they pulse
+
+// if flags do not match, revert back to solids & do not add to score - animate flags so they shake
+
+// display timer
+
+// end of game
+
+// if player finds all flags before timer ends, display WIN message
+// if player fails to find all flags before timer ends, display GAME OVER message and try again button
